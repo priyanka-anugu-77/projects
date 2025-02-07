797e33ac914a69fa1e31d7ba17452b6b36960f0e
priyanka-anugu-77 <priyanka.techie77@gmail.com>
2025-02-07 15:07:52 -0500

    Add files via upload

---
+++ login-signup/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './components/LoginSignupComponent/LoginSignupComponent';
+
+
+function App() {
+  return (
+    <div >
+      <LoginSignup></LoginSignup>
+    </div>
+  );
+}
+
+export default App;
